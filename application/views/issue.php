2d52161415d95d68f406108e604c0df506f2c4b6
Toph Tucker <tophtucker@gmail.com>
2012-06-21 05:26:58 -0400

    add google analytics; speed up smooth scroll; test carousel tile h3 leftline
    carousel h3 { border-left } was then disabled until i can make it look
    good.

--- application/views/issue.php
+++ application/views/issue.php
@@ -33,12 +33,29 @@
     {
         // Set up localScroll smooth scroller to scroll the whole document
         $('#mainnav').localScroll({
-           target:'body'
+           target:'body',
+           duration: '1000' //uh, not sure this is working!
         });
  		
     });
     
     </script>
+	
+	<!-- Google Analytics -->
+	<script type="text/javascript">
+	
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-18441903-3']);
+	  _gaq.push(['_trackPageview']);
+	
+	  (function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	
+	</script>
+	<!-- End Google Analytics -->
 	
 </head>
 
