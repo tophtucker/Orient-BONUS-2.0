1cdb63f169190a25c80310ec9906d2e1f6ac03de
Toph Tucker <tophtucker@gmail.com>
2012-07-29 02:27:08 -0400

    messing with the footer

--- application/views/bodyfooter.php
+++ application/views/bodyfooter.php
@@ -1,35 +1,26 @@
 	<footer id="bodyfooter">
 		
-		<div id="footerlinks">
-			About<br/>
-			Archives<br/>
-			Subscribe<br/>
-			Advertise<br/>
-			Submit a tip
-		</div>
-		
 		<div id="vcard" class="vcard">
 			<a class="fn org url" href="http://orient.bowdoin.edu" title="The Bowdoin Orient"><span class="organization-name">The Bowdoin Orient</span></a><br>
 			<span class="adr">
+				<span class="email"><a href="mailto:orient@bowdoin.edu">orient@bowdoin.edu</a></span><br>
+				<span class="tel"><span class="value">(207) 725-3300</span></span><br>
 				<span class="street-address">6200 College Station</span><br>
 				<span class="locality">Brunswick</span>, <span class="region">Maine</span> <span class="postal-code">04011</span><br>
-				<span class="email"><a href="mailto:orient@bowdoin.edu">orient@bowdoin.edu</a></span><br>
-				<span class="tel"><span class="hidetablet">Telephone: </span><span class="value">(207) 725-3300</span></span><br>
-				<span class="tel"><span class="hidetablet">Business phone: </span><span class="value">(207) 725-3053</span></span>
 			</span>
+			<p id="copyright"><small>&copy; <?=date("Y")?>, The Bowdoin Orient</a>. &ndash;30&ndash;</small></p>
 		</div>
-
+		
+		<div id="footerlinks">
+			<a href="<?=site_url()?>/pages/about">About</a><br/>
+			<a href="<?=site_url()?>/pages/archives">Archives</a><br/>
+			<a href="<?=site_url()?>/pages/subscribe">Subscribe</a><br/>
+			<a href="<?=site_url()?>/pages/advertise">Advertise</a><br/>
+		</div>
+		
 		<div id="bonusquoteblock">
 			<p class="bonusquote">&ldquo;<?=$quote->quote?>&rdquo;</p>
 			<p class="bonusquoteattribution">&mdash; <?=$quote->attribution?></p>
-		</div>
-		
-		<div id="copyright">
-			<small>&copy; <?=date("Y")?>, The Bowdoin Orient</a></small>
-		</div>
-		
-		<div id="thirty">
-			<small>&ndash;30&ndash;</small>
 		</div>
 		
 	</footer>
