243e2cde8ab7599945cfedc09e6db6d7c48b9458
Toph Tucker <tophtucker@gmail.com>
2012-09-23 17:22:48 -0400

    rss: str_replace with xml entities

--- application/views/rss.php
+++ application/views/rss.php
@@ -1,15 +1,21 @@
-<?= '<?xml version="1.0"?>' ?>
-<rss version="2.0">
+<?='<?xml version="1.0"?><rss version="2.0">'?> 
+<?php
+function xmlclean($string) {
+	return str_replace(array("&", "<", ">", "\"", "'"),
+        array("&amp;", "&lt;", "&gt;", "&quot;", "&apos;"), $string);
+}
+?>
   <channel>
-    <title><?= $title ?></title>
+    <title><?= xmlclean($title) ?></title>
     <link><?= $url ?></link>
-    <description><?= $description ?></description>
+    <description><?= xmlclean($description) ?></description>
     <? foreach($articles as $article): ?>
     <item>
-       <title><?= $article->title ?></title>
+       <guid><?= $article->id ?></guid>
+       <title><?= xmlclean($article->title) ?></title>
        <link>http://bowdoinorient.com/article/<?= $article->id ?></link>
        <pubDate><?= gmdate(DATE_RSS, strtotime($article->date)); ?></pubDate>
-       <description><?= $article->pullquote ?></description>
+       <description><?= xmlclean($article->pullquote) ?></description>
     </item>
     <? endforeach; ?>
   </channel>
