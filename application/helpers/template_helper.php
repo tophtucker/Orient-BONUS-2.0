63c181f01e77c10bf34b3e5044a6e8facd32eb57
Toph Tucker <tophtucker@gmail.com>
2012-09-26 17:51:07 -0400

    first step toward Sane Timestamping Everywhere®
    Deans both Foster and Levy have complained that it's too hard to tell
    what's new, and they're totally right. Sane and humane, baby. STE.

--- application/helpers/template_helper.php
+++ application/helpers/template_helper.php
@@ -49,4 +49,30 @@ function issue($p1, $p2=false)
 	return true;
 }
 
+// from http://snipplr.com/view/35635/
+function relativedate($secs) {
+	$second = 1;
+	$minute = 60;
+	$hour = 60*60;
+	$day = 60*60*24;
+	$week = 60*60*24*7;
+	$month = 60*60*24*7*30;
+	$year = 60*60*24*7*30*365;
+	
+	if ($secs <= 0) { $output = "now";
+	}elseif ($secs > $second && $secs < $minute) { $output = round($secs/$second)." second";
+	}elseif ($secs >= $minute && $secs < $hour) { $output = round($secs/$minute)." minute";
+	}elseif ($secs >= $hour && $secs < $day) { $output = round($secs/$hour)." hour";
+	}elseif ($secs >= $day && $secs < $week) { $output = round($secs/$day)." day";
+	}elseif ($secs >= $week && $secs < $month) { $output = round($secs/$week)." week";
+	}elseif ($secs >= $month && $secs < $year) { $output = round($secs/$month)." month";
+	}elseif ($secs >= $year && $secs < $year*10) { $output = round($secs/$year)." year";
+	}else{ $output = " more than a decade ago"; }
+	
+	if ($output <> "now"){
+		$output = (substr($output,0,2)<>"1 ") ? $output."s" : $output;
+	}
+	return $output;
+}
+
 ?>
