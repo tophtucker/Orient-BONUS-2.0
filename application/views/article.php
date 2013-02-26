be00cc7e9054baaa9b85307932ad98af5a95170e
Toph Tucker <tophtucker@gmail.com>
2013-02-26 04:39:07 -0500

    chartbeat

--- application/views/article.php
+++ application/views/article.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+	<script type="text/javascript">var _sf_startpt=(new Date()).getTime()</script>
 	<meta charset="utf-8" />
 	<title><?=$article->title?> &mdash; The Bowdoin Orient</title>
 	<link rel="shortcut icon" href="<?=base_url()?>images/o-32-transparent.png">
@@ -983,6 +984,25 @@ $(document).ready(function(){
 	
 	</script>
 <? endif; ?>
+
+<script type="text/javascript">
+  var _sf_async_config = { uid: 45947, domain: 'bowdoinorient.com' };
+  (function() {
+    function loadChartbeat() {
+      window._sf_endpt = (new Date()).getTime();
+      var e = document.createElement('script');
+      e.setAttribute('language', 'javascript');
+      e.setAttribute('type', 'text/javascript');
+      e.setAttribute('src',
+        (("https:" == document.location.protocol) ? "https://a248.e.akamai.net/chartbeat.download.akamai.com/102508/" : "http://static.chartbeat.com/") +
+        "js/chartbeat.js");
+      document.body.appendChild(e);
+    };
+    var oldonload = window.onload;
+    window.onload = (typeof window.onload != 'function') ?
+      loadChartbeat : function() { oldonload(); loadChartbeat(); };
+  })();
+</script>
 
 </body>
 
